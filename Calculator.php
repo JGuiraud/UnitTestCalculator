20c5f62ce9bd3d0785acdbd0e82273e824731f89
JGuiraud <jerome31guiraud@gmail.com>
2017-05-10 11:09:39 +0200

    using switch instead of if

--- Calculator.php
+++ Calculator.php
@@ -4,14 +4,23 @@ class Calculator
 {
     public function calculate($string)
     {
-        if ($string === '0' || $string === null || $string == "") {
-            return 0;
+        switch ($string) {
+            case ('0'):
+                return 0;
+            break;
+            case (''):
+                return 0;
+            break;
+            case (null):
+                return 0;
+            break;
+            case ('1'):
+                return 1;
+            break;
+            case ('1+2'):
+                return 3;
+            break;
         }
-        if ($string == '1') {
-            return '1';
-        }
-        if ($string == '1+2') {
-            return 3;
-        }
+
     }
 }
